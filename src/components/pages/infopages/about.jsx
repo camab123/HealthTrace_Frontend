4bc93b8ba906f8d9621883215eafccc3ad6abfae
Cameron Abeel <abeelcf@Camerons-MacBook-Pro-2.local>
2021-12-20 23:28:06 -0500

    footer update and about page

--- src/components/pages/infopages/about.jsx
+++ src/components/pages/infopages/about.jsx
@@ -10,19 +10,23 @@ function AboutContainer() {
                         Our Goal
                     </div>
                     <div className="GoalDetail">
-                        To provide the public with data on the transactions from pharmaceutical companies
+                        To present individuals and researchers with as much information as possible
                     </div>
                 </div>
             </div>
             <div className="AboutPage color-5-background">
                 <div className="MainBox AboutBox">
-                    <p>At WhatsUpDoc it is our goal to make the healthcare industry data more accessible and useable for every person. 
-                        The data collected and used on this platform was collected from government datasets and was not altered in any.
-                        Our data is up to date with the latest releases from <a href="https://www.cms.gov">cms.gov</a>.
-                        This work started from a passion for data and the a curiosity for the healthcare industry. We have the upmost respect for
-                        healthcare workers, doctors, and pharmaceutical companies that provide us with the many necessary and needed drugs. This website
-                        is not meant to besmirch the name of any individual or company, but to help make this information public.
+                    <p>
+                    &emsp;&emsp;At HealthTrace it is our goal to make the healthcare industry's data more accessible and useable for every person. 
+                        This work started from a passion for research and the a curiosity for the healthcare industry. We have the upmost respect for
+                        healthcare workers, doctors, and pharmaceutical companies that provide us with the many necessary and needed drugs and devices. It is our hope that through this data
+                        we can find faults in our current system that might lead to learning more about drug targeting and pharmaceutical marketing strategies. The data collected and used on this platform was collected from government datasets and was not altered in any.
+                        Our data is up to date with the latest releases from <a href="https://www.cms.gov">cms.gov</a>. All pages for Doctors, Companies,
+                        and States were created through the database of data gathered from CMS. No individual page or information has been altered in a way to mislead
+                        users. Any discrepancies in the data should be reported to Centers for Medicare and Medicaid Services to update their records.
                     </p>
+                        <div className='right'>Thank you for your support,</div>
+                        <div className='right'>Cameron Abeel</div>
                 </div>
             </div>
         </>
